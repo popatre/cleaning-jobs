f1468e34ab95b8220a76b374c020d8f286870137
popatre <76402245+popatre@users.noreply.github.com>
2025-03-01 08:49:40 +0000

    add placeholder files for other mock data

--- aws-serverless/db/data/test-data/index.ts
+++ aws-serverless/db/data/test-data/index.ts
@@ -2,3 +2,6 @@ export { companyData } from "./company";
 export { driverData } from "./drivers";
 export { jobData } from "./job";
 export { serviceTypesData } from "./servicesTypes";
+export { wasteCollection } from "./wasteCollection";
+export { otherService } from "./otherService";
+export { serviceItem } from "./serviceItem";
